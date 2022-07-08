38e87f11921dd82eb463460aa4a7a9ddc0e490f7
Дмитрий Данилов <kertarus777@mail.ru>
2022-07-08 06:57:46 +0500

    Исправлено в соответствии с PEP8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,16 +1,13 @@
-from multiprocessing import context
-from re import template
-from django.forms import SlugField
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Post, Group
+
 
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
     }
-    return render(request, 'posts/index.html', context) 
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
