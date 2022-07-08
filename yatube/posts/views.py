e6736af877b83f5f267ec011e1594ceb61a9d02e
Дмитрий Данилов <kertarus777@mail.ru>
2022-07-08 06:38:09 +0500

    На проверку

---
+++ yatube/posts/views.py
@@ -0,0 +1,24 @@
+from multiprocessing import context
+from re import template
+from django.forms import SlugField
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    template = 'posts/group_list.html'
+    return render(request, template, context)
